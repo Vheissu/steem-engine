d86a39fe0459b41a5f2f7736f939c59a302ec83a
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-28 20:24:42 +1000

    feat(renderer): improvements

--- projects/src/resources/bootstrap-form-renderer.ts
+++ projects/src/resources/bootstrap-form-renderer.ts
@@ -1,60 +1,68 @@
+import { ValidationController } from 'aurelia-validation';
+import { Container } from 'aurelia-dependency-injection';
+import { ValidationControllerFactory } from 'aurelia-validation';
 import {
     ValidationRenderer,
     RenderInstruction,
     ValidateResult
-} from 'aurelia-validation';
-
-export class BootstrapFormRenderer {
+  } from 'aurelia-validation';
+  
+  export class BootstrapFormRenderer {
     render(instruction: RenderInstruction) {
-        for (let { result, elements } of instruction.unrender) {
-            for (let element of elements) {
-                this.remove(element, result);
-            }
+      
+      for (let { result, elements } of instruction.unrender) {
+        for (let element of elements) {
+          this.remove(element, result);
         }
-
-        for (let { result, elements } of instruction.render) {
-            for (let element of elements) {
-                this.add(element, result);
-            }
+      }
+  
+      for (let { result, elements } of instruction.render) {
+        for (let element of elements) {
+          this.add(element, result);
         }
+      }
     }
-
+  
     add(element: Element, result: ValidateResult) {
-        if (result.valid) {
-            if (!element.classList.contains('is-invalid')) {
-                element.classList.add('is-valid');
-            }
-        } else {
-            // add the has-error class to the enclosing form-group div
-            element.classList.remove('is-valid');
-            element.classList.add('is-invalid');
+      if (result.valid) {
+        return;
+      } 
 
-            // add help-block
-            const message = document.createElement('span');
-            message.className = 'invalid-feedback validation-message';
-            message.textContent = result.message;
-            message.id = `validation-message-${result.id}`;
-            element.parentElement.appendChild(message);
-        }
+      // add the is-invalid class to the enclosing form-group div
+      element.classList.add('is-invalid');
+  
+      const formGroup = element.closest('.form-group');      
+      if (!formGroup) {
+        return;
+      }
+
+      // add help-block
+      const message = document.createElement('div');
+      message.className = 'invalid-feedback';
+      message.textContent = result.message;
+      message.id = `validation-message-${result.id}`;
+      formGroup.appendChild(message);
     }
-
+  
     remove(element: Element, result: ValidateResult) {
-        if (result.valid) {
-            if (element.classList.contains('is-valid')) {
-                element.classList.remove('is-valid');
-            }
-        } else {
-            // remove help-block
-            const message = element.parentElement.querySelector(
-                `#validation-message-${result.id}`
-            );
-            if (message) {
-                element.parentElement.removeChild(message);
-
-                if (element.parentElement.querySelectorAll('validation-message').length === 0) {
-                    element.classList.remove('is-invalid');
-                }
-            }
+      if (result.valid) {
+        return;
+      }
+  
+      const formGroup = element.closest('.form-group');
+      if (!formGroup) {
+        return;
+      }
+  
+      // remove help-block
+      const message = formGroup.querySelector(`#validation-message-${result.id}`);
+      if (message) {
+        formGroup.removeChild(message);
+  
+        // remove the is-invalid class from the enclosing form-group div
+        if (formGroup.querySelectorAll('.invalid-feedback').length === 0) {
+          element.classList.remove('is-invalid');
         }
+      }
     }
 }
