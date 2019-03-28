254039c699a3b23d1ef5d84070349bcd48859203
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-28 17:01:54 +1000

    feat(validation): step 1 rule keys for localisation

--- projects/src/routes/kyc/step-1.rules.ts
+++ projects/src/routes/kyc/step-1.rules.ts
@@ -1,6 +1,6 @@
 import { ValidationRules } from 'aurelia-validation';
 
 export const Step1Rules = ValidationRules
-    .ensure('firstName').required()
-    .ensure('lastName').required()
+    .ensure('firstName').required().withMessageKey('firstName')
+    .ensure('lastName').required().withMessageKey('lastName')
     .rules;
