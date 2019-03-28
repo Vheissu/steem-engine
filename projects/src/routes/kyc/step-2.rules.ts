e7d1dc5cab37657ccd2cc6455268a1a077647930
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-28 17:00:41 +1000

    feat(validation): step 2 contactmethod alternative rule if other selected

--- projects/src/routes/kyc/step-2.rules.ts
+++ projects/src/routes/kyc/step-2.rules.ts
@@ -2,4 +2,6 @@ import { ValidationRules } from 'aurelia-validation';
 
 export const Step2Rules = ValidationRules
     .ensure('contactMethod').required()
+    .ensure('contactMethodAlternative').required()
+    .when((o: any) => o.contactMethod === 'other')
     .rules;
