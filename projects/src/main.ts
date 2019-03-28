6f88865e3acfaf07035c3c887d33d612a6c5c44a
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-28 10:50:16 +1000

    feat(localisation): support different languages

--- projects/src/main.ts
+++ projects/src/main.ts
@@ -4,6 +4,8 @@ import {Aurelia} from 'aurelia-framework';
 import environment from './environment';
 import {PLATFORM} from 'aurelia-pal';
 
+import { I18N, TCustomAttribute } from 'aurelia-i18n';
+import Backend from 'i18next-xhr-backend';
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
@@ -17,5 +19,25 @@ export function configure(aurelia: Aurelia) {
 
   aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-i18n'), instance => {
+        let aliases = ['t', 'i18n'];
+        TCustomAttribute.configureAliases(aliases);
+
+        // register backend plugin
+        instance.i18next.use(Backend);
+
+        return instance.setup({
+            backend: {
+                loadPath: './locales/{{lng}}/{{ns}}.json'
+            },
+            attributes: aliases,
+            lng: environment.defaultLocale,
+            ns: ['translation', 'headings', 'buttons'],
+            defaultNS: 'translation',
+            fallbackLng: 'en',
+            debug: false
+        });
+    });
+
+    aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
