a2f97b4ed77706581b0fef502a5b453ddb5d161e
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-28 19:36:37 +1000

    feat(step-2): i18n keys

--- projects/src/routes/kyc/step-2.rules.ts
+++ projects/src/routes/kyc/step-2.rules.ts
@@ -1,7 +1,7 @@
 import { ValidationRules } from 'aurelia-validation';
 
 export const Step2Rules = ValidationRules
-    .ensure('contactMethod').required()
-    .ensure('contactMethodAlternative').required()
+    .ensure('contactMethod').required().withMessageKey('contactMethod')
+    .ensure('contactMethodAlternative').required().withMessageKey('contactMethodAlternative')
     .when((o: any) => o.contactMethod === 'other')
     .rules;
