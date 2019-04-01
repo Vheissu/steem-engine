902b554c1e2f928f3e8b5914d43aea49ab039b66
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-01 12:20:30 +1000

    feat(app): copy environment values from main site

--- projects/aurelia_project/environments/prod.ts
+++ projects/aurelia_project/environments/prod.ts
@@ -3,5 +3,12 @@ import { baseEnvironmentConfiguration } from 'base-environment';
 export default {
     ...baseEnvironmentConfiguration,
     debug: false,
-    testing: false
+    testing: false,
+    MAINTENANCE_MODE: false,
+	CHAIN_ID: 'ssc-mainnet1',
+	RPC_URL: 'https://api.steem-engine.com/rpc',
+	ACCOUNTS_API_URL: 'https://api.steem-engine.com/accounts',
+	NATIVE_TOKEN: 'ENG',
+	STEEMP_ACCOUNT: 'steem-peg',
+	PEGGED_TOKEN: 'STEEMP'
 } as any;
