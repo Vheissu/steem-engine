477891af92f0fe835ed19a3c0472572a4bf23e7b
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-01 12:51:34 +1000

    feat(services): login and logout methods in service

--- projects/src/services/steem-engine.ts
+++ projects/src/services/steem-engine.ts
@@ -3,7 +3,9 @@ import { HttpClient } from 'aurelia-fetch-client';
 import { lazy } from 'aurelia-framework';
 import environment from 'environment';
 import SSC from 'sscjs';
-import { connectTo } from 'aurelia-store';
+import { connectTo, dispatchify } from 'aurelia-store';
+import steem from 'steem';
+import { logout } from 'store/actions';
 
 @connectTo()
 export class SteemEngine {
@@ -28,6 +30,64 @@ export class SteemEngine {
         this.http.fetch(url, {
             method: 'GET'
         })
+    }
+
+    async login(username: string, key: string) {
+        if (window.steem_keychain && !key) {
+		    steem_keychain.requestSignBuffer(username, 'Log In', 'Posting', function(response) {
+				if(response.error) {
+                    //SE.ShowToast(false, 'Unable to log in with the @' + username + ' account.');
+				} else {
+					localStorage.setItem('username', username);
+					window.location.reload();
+				}
+			});
+        } else {
+			try {
+				if (key && !steem.auth.isWif(key)) {
+					key = steem.auth.getPrivateKeys(username, key, ['posting']).posting;
+				}
+			} catch(err) {
+                //SE.ShowToast(false, 'Invalid private key or master password.');
+				return;
+            }
+
+            try {
+                const user = await steem.api.getAccountsAsync([username]);
+
+                if (user && user.length > 0) {
+					try {
+						if (steem.auth.wifToPublic(key) == user[0].memo_key || steem.auth.wifToPublic(key) === user[0].posting.key_auths[0][0]) {
+							localStorage.setItem('username', username);
+							localStorage.setItem('key', key);
+							window.location.reload();
+						} else {
+                            //SE.ShowToast(false, 'Unable to log in with the @' + username + ' account. Invalid private key or password.');
+						}
+					} catch(err) {
+                        //SE.ShowToast(false, 'Unable to log in with the @' + username + ' account. Invalid private key or password.');
+					}
+                } else {
+                    //SE.ShowToast(false, 'There was an error loading the @' + username + ' account.');
+                }
+            } catch (e) {
+                return;
+            }
+        }
+    }
+
+    logout() {
+        dispatchify(logout)();
+    }
+
+    async getAccount(username: string) {
+        try {
+            const user = await steem.api.getAccountsAsync([username]); 
+        
+            return user && user.length > 0 ? user[0] : null;
+        } catch (e) {
+            throw new Error(e);
+        }
     }
 
     async loadBalances(account: string) {
