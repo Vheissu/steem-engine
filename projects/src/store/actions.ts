f8edfd25a961ee26db91efeee36e4a3605831571
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-01 12:21:48 +1000

    feat(store): actions for getting balances and tokens

--- projects/src/store/actions.ts
+++ projects/src/store/actions.ts
@@ -1,5 +1,9 @@
+import { SteemEngine } from 'services/steem-engine';
+import { Container } from 'aurelia-framework';
 import { State } from './state';
 import store from './store';
+
+const SE = Container.instance.get(SteemEngine);
 
 export async function login(state: State, user: any): Promise<State> {
     let newState = { ...state };
@@ -41,6 +45,31 @@ export async function setUserMeta(state: State, data: any): Promise<State> {
     return newState;
 }
 
+export async function loadBalances(state: State, username: string): Promise<State> {
+    let newState = { ...state };
+
+    const balances = await SE.loadBalances(username);
+
+    newState.balances = balances;
+
+    return newState;
+}
+
+export async function getToken(state: State, token: string) {
+    let newState = { ...state };
+    
+    if (newState.balances) {
+        const token = newState.balances.find(b => b.symbol === token);
+        newState.token = token ? parseFloat(token.balance) : 0;
+    } else {
+        newState.token = 0;
+    }
+
+    return newState;
+}
+
 store.registerAction('login', login);
 store.registerAction('logout', logout);
 store.registerAction('setUserMeta', setUserMeta);
+store.registerAction('loadBalances', loadBalances);
+store.registerAction('getToken', getToken);
