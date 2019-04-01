490be51f38a58d72e4a68841989a4ba10bf147e4
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-01 12:21:36 +1000

    chore(environment): generated config

--- projects/src/environment.ts
+++ projects/src/environment.ts
@@ -3,5 +3,12 @@ import { baseEnvironmentConfiguration } from 'base-environment';
 export default {
     ...baseEnvironmentConfiguration,
     debug: true,
-    testing: true
+    testing: true,
+	MAINTENANCE_MODE: false,
+	CHAIN_ID: 'ssc-00000000000000000002',
+	RPC_URL: 'https://testapi.steem-engine.com/',
+	ACCOUNTS_API_URL: 'https://testaccounts.steem-engine.com',
+	NATIVE_TOKEN: 'SSC',
+	STEEMP_ACCOUNT: 'steemsc',
+	PEGGED_TOKEN: 'STEEMP'
 } as any;
