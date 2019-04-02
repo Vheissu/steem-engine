80e2bd0faf60ef53e920891eb8967975b1bdb142
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-03 08:55:35 +1000

    feat(app): environments updated with new values

--- projects/aurelia_project/environments/dev.ts
+++ projects/aurelia_project/environments/dev.ts
@@ -2,13 +2,36 @@ import { baseEnvironmentConfiguration } from 'base-environment';
 
 export default {
     ...baseEnvironmentConfiguration,
-    debug: true,
-    testing: true,
+    debug: false,
+    testing: false,
 	MAINTENANCE_MODE: false,
 	CHAIN_ID: 'ssc-00000000000000000002',
 	RPC_URL: 'https://testapi.steem-engine.com/',
 	ACCOUNTS_API_URL: 'https://testaccounts.steem-engine.com',
+	CONVERTER_API: 'https://converter-api.steem-engine.com/api',
 	NATIVE_TOKEN: 'SSC',
 	STEEMP_ACCOUNT: 'steemsc',
-	PEGGED_TOKEN: 'STEEMP'
+	PEGGED_TOKEN: 'STEEMP',
+	PEGGED_TOKENS: [
+		{
+			name: 'Bitcoin',
+			symbol: 'BTC',
+			pegged_token_symbol: 'BTCP'
+		}, 
+		{
+			name: 'Litecoin',
+			symbol: 'LTC',
+			pegged_token_symbol: 'LTCP'
+		}, 
+		{
+			name: 'Bitcoin Cash',
+			symbol: 'BCH',
+			pegged_token_symbol: 'BCHP'
+		}, 
+		{
+			name: 'Dogecoin',
+			symbol: 'DOGE',
+			pegged_token_symbol: 'DOGEP'
+		}
+	]
 } as any;
