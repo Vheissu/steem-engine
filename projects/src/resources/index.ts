38c9ab08dcdb3a23b24cceb93cccdabb06713de3
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-02 16:44:27 +1000

    feat(resources): comma value converter

--- projects/src/resources/index.ts
+++ projects/src/resources/index.ts
@@ -5,5 +5,6 @@ export function configure(config: FrameworkConfiguration) {
     config.globalResources([
         PLATFORM.moduleName('./attributes/flatpickr'),
         PLATFORM.moduleName('./value-converters/large-number'),
+        PLATFORM.moduleName('./value-converters/comma'),
     ]);
 }
