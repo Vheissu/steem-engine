3b879ba1ce77ce627a2c7e1813919d340462bf03
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-02 17:00:53 +1000

    feat(modals): send modal scoped

--- projects/src/modals/send.ts
+++ projects/src/modals/send.ts
@@ -3,8 +3,15 @@ import { autoinject } from 'aurelia-framework';
 
 @autoinject()
 export class SendModal {
+    private token;
+    private transaction;
+
     constructor(private controller: DialogController) {
         this.controller.settings.lock = false;
         this.controller.settings.centerHorizontalOnly = true;
+    }
+
+    activate(token) {
+        this.token = token;
     }
 }
