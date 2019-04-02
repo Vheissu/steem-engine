7ddf0caab37046bfbe7d26dbd4121026f9dca972
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-02 12:13:25 +1000

    feat(store): mutate balances for display

--- projects/src/store/store.ts
+++ projects/src/store/store.ts
@@ -1,10 +1,13 @@
 import { Container } from 'aurelia-framework';
-import { localStorageMiddleware, Store, MiddlewarePlacement, rehydrateFromLocalStorage } from 'aurelia-store';
+import { localStorageMiddleware, Store, MiddlewarePlacement, rehydrateFromLocalStorage, CallingAction } from 'aurelia-store';
 import { State } from './state';
+
+import { BalancesMiddleware } from './middleware/balances';
 
 const store: Store<State> = Container.instance.get(Store);
 
 store.registerMiddleware(localStorageMiddleware, MiddlewarePlacement.After, { key: 'steem-engine__state' });
+store.registerMiddleware(BalancesMiddleware, MiddlewarePlacement.After);
 
 store.registerAction('Rehydrate', rehydrateFromLocalStorage);
 
