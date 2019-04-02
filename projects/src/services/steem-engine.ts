0c3cc35ea04ac9f756772af9bee428055a7373dc
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-02 21:29:21 +1000

    feat(services): steem engine class methods and price functionality

--- projects/src/services/steem-engine.ts
+++ projects/src/services/steem-engine.ts
@@ -1,10 +1,11 @@
+import { Subscription } from 'rxjs';
 import { I18N } from 'aurelia-i18n';
 import { State } from 'store/state';
 import { HttpClient } from 'aurelia-fetch-client';
 import { lazy } from 'aurelia-framework';
 import environment from 'environment';
 import SSC from 'sscjs';
-import { connectTo, dispatchify } from 'aurelia-store';
+import { connectTo, dispatchify, Store } from 'aurelia-store';
 import steem from 'steem';
 import { logout } from 'store/actions';
 
@@ -14,20 +15,33 @@ import { queryParam, popupCenter, tryParse } from 'common/functions';
 @connectTo()
 export class SteemEngine {
     private http: HttpClient;
+    private http2: HttpClient;
     private ssc;
     private state: State;
+    private subscription: Subscription;
 
     constructor(
         @lazy(HttpClient) private getHttpClient: () => HttpClient,
         private i18n: I18N,
+        private store: Store<State>,
         private toast: ToastService) {
         this.http = getHttpClient();
+        this.http2 = getHttpClient();
+
         this.ssc = new SSC(environment.RPC_URL);
 
         this.http.configure(config => {
             config
                 .useStandardConfiguration()
                 .withBaseUrl(environment.ACCOUNTS_API_URL);
+        });
+
+        this.http2.configure(config => config.useStandardConfiguration());
+
+        this.subscription = this.store.state.subscribe((state: State) => {
+            if (state) {
+                this.state = state;
+            }
         });
     }
 
@@ -42,86 +56,99 @@ export class SteemEngine {
         })
     }
 
-    async login(username: string, key: string) {
-        if (window.steem_keychain && !key) {
-		    steem_keychain.requestSignBuffer(username, 'Log In', 'Posting', function(response) {
-				if (response.error) {
-                    const toast = new ToastMessage();
+    async loadSteemPrice() {
+        const request = await this.http2.fetch('https://postpromoter.net/api/prices', {
+            method: 'get'
+        });
 
-                    toast.message = this.i18n.tr('errorLogin', { 
-                        username, 
+        const response = await request.json();
+
+        return parseFloat(response.steem_price);
+    }
+
+    async login(username: string, key?: string) {
+        return new Promise(async (resolve, reject) => {
+            if (window.steem_keychain && !key) {
+                steem_keychain.requestSignBuffer(username, 'Log In', 'Posting', function(response) {
+                    if (response.error) {
+                        const toast = new ToastMessage();
+    
+                        toast.message = this.i18n.tr('errorLogin', { 
+                            username, 
+                            ns: 'errors' 
+                        });
+    
+                        this.toast.error(toast);
+                    } else {
+                        localStorage.setItem('username', username);
+                        resolve(username);
+                    }
+                });
+            } else {
+                try {
+                    if (key && !steem.auth.isWif(key)) {
+                        key = steem.auth.getPrivateKeys(username, key, ['posting']).posting;
+                    }
+                } catch(err) {
+                    const toast = new ToastMessage();
+    
+                    toast.message = this.i18n.tr('invalidPrivateKeyOrPassword', { 
                         ns: 'errors' 
                     });
-
+    
                     this.toast.error(toast);
-				} else {
-					localStorage.setItem('username', username);
-					window.location.reload();
-				}
-			});
-        } else {
-			try {
-				if (key && !steem.auth.isWif(key)) {
-					key = steem.auth.getPrivateKeys(username, key, ['posting']).posting;
-				}
-			} catch(err) {
-                const toast = new ToastMessage();
+                    return;
+                }
+    
+                try {
+                    const user = await steem.api.getAccountsAsync([username]);
+    
+                    if (user && user.length > 0) {
+                        try {
+                            if (steem.auth.wifToPublic(key) == user[0].memo_key || steem.auth.wifToPublic(key) === user[0].posting.key_auths[0][0]) {
+                                localStorage.setItem('username', username);
+                                localStorage.setItem('key', key);
 
-                toast.message = this.i18n.tr('invalidPrivateKeyOrPassword', { 
-                    ns: 'errors' 
-                });
-
-                this.toast.error(toast);
-				return;
-            }
-
-            try {
-                const user = await steem.api.getAccountsAsync([username]);
-
-                if (user && user.length > 0) {
-					try {
-						if (steem.auth.wifToPublic(key) == user[0].memo_key || steem.auth.wifToPublic(key) === user[0].posting.key_auths[0][0]) {
-							localStorage.setItem('username', username);
-							localStorage.setItem('key', key);
-							window.location.reload();
-						} else {
+                                resolve(username);
+                            } else {
+                                const toast = new ToastMessage();
+    
+                                toast.message = this.i18n.tr('errorLogin', { 
+                                    ns: 'errors' 
+                                });
+                
+                                this.toast.error(toast);
+                            }
+                        } catch(err) {
                             const toast = new ToastMessage();
-
+    
                             toast.message = this.i18n.tr('errorLogin', { 
                                 ns: 'errors' 
                             });
             
                             this.toast.error(toast);
-						}
-					} catch(err) {
+                        }
+                    } else {
                         const toast = new ToastMessage();
-
-                        toast.message = this.i18n.tr('errorLogin', { 
+    
+                        toast.message = this.i18n.tr('errorLoading', { 
                             ns: 'errors' 
                         });
         
                         this.toast.error(toast);
-					}
-                } else {
-                    const toast = new ToastMessage();
-
-                    toast.message = this.i18n.tr('errorLoading', { 
-                        ns: 'errors' 
-                    });
-    
-                    this.toast.error(toast);
+                    }
+                } catch (e) {
+                    return;
                 }
-            } catch (e) {
-                return;
             }
-        }
+        });
     }
 
     logout() {
         dispatchify(logout)();
     }
 
-    steemConnectJson(auth_type, data) {
+    async steemConnectJson(auth_type, data) {
         return new Promise((resolve, reject) => {
             const username = this.state.user.name;
             let url = 'https://steemconnect.com/sign/custom-json?';
@@ -197,8 +224,10 @@ export class SteemEngine {
                             }
                         }
     
-                        if (token.symbol == 'STEEMP') {
-                            token.lastPrice = 1;
+                        if (steemp_balance && steemp_balance.balance) {
+                            const token = this.getToken('STEEMP');
+                            token.supply -= parseFloat(steemp_balance.balance);
+                            token.circulatingSupply -= parseFloat(steemp_balance.balance);
                         }
                     });
     
@@ -209,9 +238,9 @@ export class SteemEngine {
                     const steemp_balance = await this.ssc.findOne('tokens', 'balances', { account: 'steem-peg', symbol: 'STEEMP' });
     
                     if (steemp_balance && steemp_balance.balance) {
-                        const token = await this.getToken('STEEMP');
-                        token.supply -= parseFloat(steemp_balance.balance);
-                        token.circulatingSupply -= parseFloat(steemp_balance.balance);
+                        //const token = await this.getToken('STEEMP');
+                        //token.supply -= parseFloat(steemp_balance.balance);
+                        //token.circulatingSupply -= parseFloat(steemp_balance.balance);
                     }
     
                     resolve(tokens);
@@ -221,39 +250,146 @@ export class SteemEngine {
     }
 
     async loadParams() {
-        // let loaded = 0;
+        let loaded = 0;
+        let params = {};
 
-		// this.ssc.findOne('sscstore', 'params', {  }, (err, result) => {
-		// 	if(result && !err)
-		// 		Object.assign(SE.Params, result);
+		this.ssc.findOne('sscstore', 'params', {  }, (err, result) => {
+			if(result && !err) {
+                params = { ...params, ...result };
+            }
 
-		// 	if(++loaded >= 3 && callback)
-		// 		callback();
-		// });
+			if(++loaded >= 3) {
+                return params;
+            }
+		});
 
-		// this.ssc.findOne('tokens', 'params', {  }, (err, result) => {
-		// 	if(result && !err)
-		// 		Object.assign(SE.Params, result);
+		this.ssc.findOne('tokens', 'params', {  }, (err, result) => {
+			if(result && !err) {
+                params = { ...params, ...result };
+            }
 
-		// 	if(++loaded >= 3 && callback)
-		// 		callback();
-		// });
+			if(++loaded >= 3) {
+                return params;
+            }
+		});
 
-		// // loadSteemPrice(() => {
-		// // 	if(++loaded >= 3 && callback)
-		// // 		callback();
-		// // });
+		this.loadSteemPrice().then(() => {
+			if(++loaded >= 3) {
+                return params;
+            }
+		});
     }
 
-    async getToken(symbol): Promise<any> {
-        return {};
-    }
-
-    // async showHistory(symbol: string, name: string) {
-    //     let token =  this.getToken(symbol);
+    async sendToken(symbol: string, to: string, quantity: number, memo: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const username = localStorage.getItem('username');
+    
+            if (!username) {
+              window.location.reload();
+              return;
+            }
         
-    //     const history = await this.request('/history', { account: this.state.user.name, limit: 100, offset: 0, type: 'user', symbol: symbol });
-    // }
+            const transaction_data = {
+              'contractName': 'tokens',
+              'contractAction': 'transfer',
+              'contractPayload': {
+                'symbol': symbol,
+                'to': to,
+                'quantity': quantity,
+                'memo': memo
+              }
+            };
+        
+            console.log('SENDING: ' + symbol);
+        
+            if (window.steem_keychain) {
+              steem_keychain.requestCustomJson(username, environment.CHAIN_ID, 'Active', JSON.stringify(transaction_data), 'Token Transfer: ' + symbol, (response) => {
+                if (response.success && response.result) {
+                    this.checkTransaction(response.result.id, 3, tx => {
+                        if (tx.success) {
+                            const toast = new ToastMessage();
+
+                            toast.message = this.i18n.tr('tokensSent', {
+                                quantity,
+                                symbol,
+                                to
+                            });
+            
+                            this.toast.success(toast);
+                        } else {
+                            const toast = new ToastMessage();
+
+                            toast.message = this.i18n.tr('errorSubmittedTransfer', {
+                                ns: 'errors',
+                                error: tx.error
+                            });
+            
+                            this.toast.error(toast);
+                            this.loadBalances(username).then(() => {
+                                this.showHistory(symbol);
+                            });
+                        }
+                    });
+                } else {
+                    // hide
+                }
+              });
+            } else {
+                    this.steemConnectJson('active', transaction_data).then(() => {
+                        // this.loadBalances(SE.User.name, () => this.showHistory(symbol));
+                    });
+                }
+        });
+    }
+
+    getBalance(token) {
+        if (this.state.user && this.state.user.balances) {
+            const token2 = this.state.user.balances.find(b => b.symbol === token);
+            return token2 ? parseFloat(token2.balance) : 0;
+        }
+
+        return 0;
+    }
+
+    getToken(symbol: string) {
+        return this.state.tokens.find(t => t.symbol === symbol);
+    }
+
+    async showHistory(symbol: string) {
+        let token =  this.getToken(symbol);
+        
+        const history = await this.request('/history', { 
+            account: this.state.user.name, 
+            limit: 100, 
+            offset: 0, 
+            type: 'user', 
+            symbol: symbol 
+        });
+    }
+
+    checkTransaction(trx_id, retries, callback) {
+		this.ssc.getTransactionInfo(trx_id, (err, result) => {
+			if (result) {
+				let error = null;
+
+				if (result.logs) {
+					const logs = JSON.parse(result.logs);
+
+					if (logs.errors && logs.errors.length > 0) {
+                        error = logs.errors[0];
+                    }
+				}
+
+				if (callback) {
+                    callback(Object.assign(result, { error: error, success: !error }));
+                }	
+			} else if (retries > 0) {
+                setTimeout(() => this.checkTransaction(trx_id, retries - 1, callback), 5000);
+            } else if (callback) {
+                callback({ success: false, error: 'Transaction not found.' });
+            }
+		});
+	}
 
     issueToken(symbol, to, quantity) {
         const transaction_data = {
