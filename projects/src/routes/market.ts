cb9ba75004c643084862022e98303c970e0be6bb
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-04-03 10:35:30 +1000

    feat(market): add in chart component

--- projects/src/routes/market.ts
+++ projects/src/routes/market.ts
@@ -1,3 +1,25 @@
 export class Market {
-    private marketDepthChart: HTMLCanvasElement;
+    private chartData = {};
+
+    attached() {
+    //     this.chartData = {
+	// 		labels: buyOrderLabels.concat(sellOrderLabels),
+	// 		datasets: [
+	// 			{
+	// 				label: 'Buy',
+	// 				steppedLine: 'after',
+	// 				borderColor: '#88e86b',
+	// 				backgroundColor: '#a9ea96',
+	// 				data: buyOrderDataset
+	// 			},
+	// 			{
+	// 				label: 'Sell',
+	// 				steppedLine: 'before',
+	// 				borderColor: '#e45858',
+	// 				backgroundColor: '#e87f7f',
+	// 				data: sellOrderDataset
+	// 			}
+	// 		]
+    //     };
+    // }
 }
